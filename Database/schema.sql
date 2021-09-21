8353a8be37e31d0d599895cd34dbd6d2f01bb4dc
Brittany Garza <brittany.garza132@gmail.com>
2021-09-20 20:57:45 -0500

    updated schema file

--- Database/schema.sql
+++ Database/schema.sql
@@ -18,6 +18,21 @@ CREATE TABLE recipe_names (
 SELECT * 
 FROM recipe_names;
 
+
+
+--Adding Primary and Foreign Keys
+
+--Adding PK and FK to Recipes_with_Cuisines
+ALTER TABLE "Recipes_with_cuisines" 
+ADD FOREIGN KEY (cuisineid) REFERENCES cuisine_names (cuisineid),
+ADD PRIMARY KEY (recipeid, cuisineid);
+
+-- Adding PK to Cuisine_with_ingredents
+ALTER TABLE "cuisine_with_ingredents"
+ADD PRIMARY KEY (recipeid)
+
+
+
 -- View tables that were imported using SQL Alchemy
 SELECT *
 FROM "Recipes_with_cuisines"
@@ -26,3 +41,19 @@ LIMIT 10;
 SELECT *
 FROM "cuisine_with_ingredents"
 LIMIT 10;
+
+
+DROP TABLE cuisine_names;
+DROP TABLE recipe_names;
+DROP TABLE "Recipes_with_cuisines";
+DROP TABLE "cuisine_with_ingredents";
+
+
+
+-- Creating the join with recipe_with_cuisine table and showing recipe and cuisine names
+SELECT * 
+FROM "Recipes_with_cuisines" AS rc
+INNER JOIN recipe_names AS rn
+ON rn.recipeid = rc.recipeid
+INNER JOIN cuisine_names as cn
+ON cn.cuisineid = rc.cuisineid;
